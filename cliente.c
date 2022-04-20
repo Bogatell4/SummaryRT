ada9c692f6d25267a8d661fc542756e48a1efa10
Sergi Bogatell <bogatell.sergi@gmail.com>
2022-04-20 11:21:13 +0200

    Initial Commit

---
+++ cliente.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <pthread.h>
+
+char buffer[256];
+char longbuffer[1024];
+struct missatge{
+	int len;
+	char *txt;
+};
+
+
+//GET MSG
+void *getmsg(){
+	char buffer[256];
+	int i=0;
+	int a=1;
+	char *msg;
+	char c;
+	struct missatge *mis1;
+	mis1=(struct missatge*)malloc(sizeof(struct missatge));
+	msg =(char*)malloc(32*sizeof(char)+1);
+	while(c!= '\n'){	
+		c=getchar();	
+		msg[i]=c;
+		i++;	
+		if (i==(32*a)-1){
+			a++;
+			msg= (char *) realloc(msg,32*a+1);	
+		}
+
+	}
+	msg[i]='\0';
+	mis1->len=i+1;
+	mis1->txt=(char*)realloc(mis1->txt,sizeof(msg)); 
+	strcpy(mis1->txt,msg);
+	sprintf(buffer,"len=%d\n",mis1->len);
+	write(1,buffer,strlen(buffer));
+	sprintf(buffer,"txt=%s",mis1->txt);
+	write(1,buffer,strlen(buffer));
+	free(msg);
+	return(mis1);
+}
+
+//REBRE MSG I ESCRIURE
+void *read_print(void *x){
+	int *sdin=(int*)x;
+	char username[32];
+	char buffer[50];
+	char buffer2[256];//fer realoc
+	char len[256];
+	char *miss;
+	int l,i=0;
+	while(i<100){
+		read(*sdin,username,32);
+		sprintf(buffer,"<%s>: ",username);
+		write(1,buffer,strlen(buffer));
+		read(*sdin,len,256);
+		miss=(char*)malloc(atoi(len));
+		read(*sdin,miss,atoi(len));
+		//sprintf(buffer2,"%s\n",miss);
+		write(1,miss,strlen(miss));
+		free(miss);
+		i++;
+	}
+}
+
+int main(int argc, char**argv){
+	//Llegir username IP i ports pel teclat
+	if (argc!=2){
+		sprintf(buffer,"ERROR input params\n");
+		write(1,buffer,strlen(buffer));
+		return 0;
+	}
+	char username[32];
+	char IP[128];
+	char portin[10];
+	char portout[10];
+	strcpy(username,argv[1]);
+	/*strcpy(IP,argv[2]);
+	strcpy(portin,argv[3]);
+	strcpy(portout,argv[4]);
+	*/
+	char IP2[128]="0.0.0.0";
+	char portin2[10]="12002";
+	char portout2[10]="11002";
+	strcpy(IP,IP2);
+	strcpy(portin,portin2);
+	strcpy(portout,portout2);
+	sprintf(buffer,"Username:%s -- IP: %s -- IN: %s -- OUT: %s \n",username, IP,portin,portout);
+	write(1,buffer,strlen(buffer));
+
+	//Creació dels 2 sockets
+	int sdin = socket(AF_INET,SOCK_STREAM,0);
+	if (sdin == -1) {
+		sprintf(buffer,"ERROR IN s.creation\n");
+		write(1,buffer,strlen(buffer));
+		return 0;
+	}else{
+		sprintf(buffer,"OK IN s.creation\n");
+		write(1,buffer,strlen(buffer));
+	}
+	int sdout = socket(AF_INET,SOCK_STREAM,0);
+	if (sdout == -1) {
+		sprintf(buffer,"ERROR OUT s.creation\n");
+		write(1,buffer,strlen(buffer));
+		return 0;
+	}else{
+		sprintf(buffer,"OK OUT s.creation\n");
+		write(1,buffer,strlen(buffer));
+	}
+
+	struct addrinfo hints,*serverinfo,*p;
+	int rvin,rvout;
+	
+	memset(&hints,0,sizeof(hints));
+	
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype=SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+	char s[INET6_ADDRSTRLEN];
+	rvout = getaddrinfo(IP,portout,&hints,&serverinfo);
+	for(p=serverinfo; p!=NULL ;p=p->ai_next){
+		inet_ntop(p->ai_family,p->ai_addr,s,sizeof(s));
+		if(connect(sdout,p->ai_addr,p->ai_addrlen)	== -1){
+			sprintf(buffer,"NO Connectat OUT\n");
+			write(1,buffer,strlen(buffer));
+			continue;
+		}
+		sprintf(buffer,"Connectat OUT\n");
+		write(1,buffer,strlen(buffer));
+		break;
+	}
+	rvin = getaddrinfo(IP,portin,&hints,&serverinfo);
+	for(p=serverinfo; p!=NULL ;p=p->ai_next){
+		inet_ntop(p->ai_family,p->ai_addr,s,sizeof(s));
+		if(connect(sdin,p->ai_addr,p->ai_addrlen)	== -1){
+			sprintf(buffer,"NO Connectat IN\n");
+			write(1,buffer,strlen(buffer));
+			continue;
+		}
+		sprintf(buffer,"Connectat IN\n");
+		write(1,buffer,strlen(buffer));
+		break;
+	}
+	freeaddrinfo(serverinfo);
+
+	write(sdout,username,strlen(username));
+
+	pthread_t id;
+	pthread_create(&id,NULL,read_print,(void*)&sdin);
+	sprintf(buffer,"thread llegir server creat\n");
+	write(1,buffer,strlen(buffer));
+	struct missatge *missatge;
+	sprintf(buffer,"Entrant bucle escriure teclat\n");
+	write(1,buffer,strlen(buffer));
+	while(1){
+		missatge=getmsg();
+		sprintf(buffer,"%d",missatge->len);
+		write(sdout,buffer,strlen(buffer));
+		sprintf(buffer,"lenbuffer sdout=%ld\n",strlen(buffer)+1);
+		write(1,buffer,strlen(buffer));
+		sleep(1);
+		write(sdout,missatge->txt,missatge->len);
+		free(missatge);
+	}
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,191 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <pthread.h>
+
+char buffer[256];
+
+struct cliente{
+	char username[32];
+	int fd_toserver;
+	int fd_toclient;
+};
+
+struct cliente clientes[100];
+pthread_mutex_t critical;
+
+struct missatge{
+	int len;
+	char *txt;
+};
+
+int createsocket(int port, struct sockaddr_in address){
+	int x;
+
+	x=socket(AF_INET,SOCK_STREAM,0);
+	if (x==-1) {
+		sprintf(buffer,"ERROR socket creation port: %d \n",port);
+		write(1,buffer,strlen(buffer));
+		return 0;
+	}else{
+		sprintf(buffer,"OK SCreation port: %d \n",port);
+		write(1,buffer,strlen(buffer));
+	}
+
+
+	if (bind(x,(struct sockaddr*) &address, sizeof(address)) == -1) {
+		sprintf(buffer,"ERROR Bind port: %d \n",port);
+		write(1,buffer,strlen(buffer));
+		return 0;
+	}else{
+		sprintf(buffer,"OK Bind port: %d \n",port);
+		write(1,buffer,strlen(buffer));
+	}
+
+	if(listen(x,2) == -1) {
+		sprintf(buffer,"ERROR listen port: %d \n",port);
+		write(1,buffer,strlen(buffer));
+		return 0;
+	}else{
+		sprintf(buffer,"OK Listen  port: %d \n",port);
+		write(1,buffer,strlen(buffer));
+	}
+	sprintf(buffer,"------------------------\n");
+	write(1,buffer,strlen(buffer));
+	return x;
+}
+
+void *read_write (void *asdf){
+	struct cliente *cliente;
+	cliente=(struct cliente*)asdf;
+	int i=0;
+	char *txt;
+	char len[256];
+	char buffer2[270];
+	sprintf(buffer2,"Thread username: %s Born\n",cliente->username);
+	write(1,buffer2,strlen(buffer2));
+	while(1){
+		read(cliente->fd_toserver,len,256);
+		sprintf(buffer2,"missatge rebut!!\n");
+		write(1,buffer2,strlen(buffer2));
+		
+		sprintf(buffer2,"len=%s\n",len);
+		write(1,buffer2,strlen(buffer2));
+		sprintf(buffer2,"%s said:",cliente->username);
+		write(1,buffer2,strlen(buffer2));
+		txt=(char*)malloc(atoi(len));
+		read(cliente->fd_toserver,txt,atoi(len));
+
+		write(1,txt,atoi(len));
+		pthread_mutex_lock(&critical);
+		while(clientes[i].fd_toserver!=0){
+			if(clientes[i].fd_toclient != cliente->fd_toclient){
+				write(clientes[i].fd_toclient,cliente->username,strlen(cliente->username));
+				sleep(1);
+				write(clientes[i].fd_toclient,len,256);
+				sleep(1);
+				write(clientes[i].fd_toclient,txt,atoi(len));
+			}
+			i++;
+		}
+		pthread_mutex_unlock(&critical);
+		free(txt);
+		i=0;
+	}
+}
+
+int main(int argc, char**argv)
+{
+
+	//Llegir els 2 ports per teclat
+	if (argc!=1){
+		sprintf(buffer,"ERROR input params\n");
+		write(1,buffer,strlen(buffer));
+		return 0;
+	}
+	/*
+	int portin=atoi(argv[1]);
+	sprintf(buffer,"primer port %d \n",atoi(argv[1]));
+	write(1,buffer,strlen(buffer));
+	int portout=atoi(argv[2]);;
+	sprintf(buffer,"segon port %d \n",atoi(argv[2]));
+	write(1,buffer,strlen(buffer));
+	*/
+	int portin=11002;
+	int portout=12002;
+
+	//Creacio de les adreces pels 2 sockets: addresin i addresout
+	int sAddrLen;
+	int sdin,sdout,j=0,in,out;
+	pthread_t id[100];
+	struct sockaddr_in addressin, addressout, retSin;
+	memset(&addressin,0,sizeof(addressin));
+	addressin.sin_family = AF_INET;
+	addressin.sin_addr.s_addr = INADDR_ANY;
+	addressin.sin_port = htons(portin);
+	memset(&addressout,0,sizeof(addressout));
+	addressout.sin_family = AF_INET;
+	addressout.sin_addr.s_addr = INADDR_ANY;
+	addressout.sin_port = htons(portout);
+
+	//Creació dels 2 sockets, retorna el sd corresponent a cada canal.
+	sdin=createsocket(portin,addressin);
+	if (sdin==0)
+	{
+		return 0;
+	}
+	sdout=createsocket(portout,addressout);
+	if (sdout==0)
+	{
+		return 0;
+	}
+
+	pthread_mutex_init(&critical,NULL);
+	sprintf(buffer,"Entrant bucle accepts\n");
+	write(1,buffer,strlen(buffer));
+	while(1){
+		sprintf(buffer,"------------------------\n");
+		write(1,buffer,strlen(buffer));
+		sprintf(buffer,"Waiting IN...\n");
+		write(1,buffer,strlen(buffer));
+		in=accept(sdin, (struct sockaddr *)&retSin, &sAddrLen);
+		if(in==-1){
+			sprintf(buffer,"IN accept error\n");
+			write(1,buffer,strlen(buffer));
+		}else{
+			sprintf(buffer,"Accepted IN\n");
+			write(1,buffer,strlen(buffer));
+			sprintf(buffer,"Waiting OUT...\n");
+			write(1,buffer,strlen(buffer));
+			out=accept(sdout, (struct sockaddr *)&retSin, &sAddrLen);
+			if(out==-1){
+				sprintf(buffer,"OUT accept error\n");
+				write(1,buffer,strlen(buffer));
+			}else{
+				sprintf(buffer,"Accepted OUT:\n");
+				write(1,buffer,strlen(buffer));
+				pthread_mutex_lock(&critical);
+				read(in,clientes[j].username,32);
+				clientes[j].fd_toclient=out;
+				clientes[j].fd_toserver=in;
+				sprintf(buffer,"Creating thread...\n");
+				write(1,buffer,strlen(buffer));
+				pthread_create(&id[j],NULL,read_write,(void *)&clientes[j]);
+				pthread_mutex_unlock(&critical);
+				j++;
+			}
+		}
+	}
+	return 0;
+}
+
